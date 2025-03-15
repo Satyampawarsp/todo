2ea25b4e43903f0e8b37216c18687330017beed6
satyam1733 <satyampawar1733@gmail.com>
2025-03-15 13:51:50 +0530

    remove extra one

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import TodoList from './components/todolist'
 import List from './components/List'
 
 const App = () => {
   return (
     <div>
       <List/>
-      {/* <TodoList/> */}
     </div>
   )
 }
